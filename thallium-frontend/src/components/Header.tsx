268b4539174c94d72aff085e195479475366d1b4
Joe Banks <joe@jb3.dev>
2024-08-17 02:05:54 +0100

    Split Header to independent component

---
+++ thallium-frontend/src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 2rem;
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+
+  h1 {
+    margin: 0;
+    margin-left: 1rem;
+  }
+`;
+
+
+const Header = () => {
+    return (
+        <HeaderContainer>
+            <img src="icon.svg" alt="Thallium logo" />
+            <h1>
+                Thallium
+            </h1>
+        </HeaderContainer>
+    );
+};
+
+export default Header;
