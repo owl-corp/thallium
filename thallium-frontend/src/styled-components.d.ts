56fa4745ff7a4c493551aea2e4e0b87b220b7dbb
Joe Banks <joe@jb3.dev>
2024-08-19 17:05:07 +0100

    Add new CSSProp support in styled-components typing

--- thallium-frontend/src/styled-components.d.ts
+++ thallium-frontend/src/styled-components.d.ts
@@ -1,5 +1,12 @@
 import type { Theme } from "./themes";
+import { CSSProp } from "styled-components";
 
 declare module "styled-components" {
     export interface DefaultTheme extends Theme { }
+}
+
+declare module "react" {
+    interface Attributes {
+        css?: CSSProp;
+    }
 }
