a64d57e5d60f283df266ed8000ce6262656a3bba
Chris Lovering <chris.lovering.95@gmail.com>
2024-08-24 19:10:58 +0100

    Add voucher tests

--- thallium-backend/tests/test_login.py
+++ thallium-backend/tests/test_login.py
@@ -2,13 +2,18 @@ from http import HTTPStatus
 
 import pytest
 from httpx import AsyncClient
+from pytest_subtests import SubTests
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.orm import Voucher
 
 
 @pytest.mark.asyncio()
-async def test_successful_voucher_login(http_client: AsyncClient, db_session: AsyncSession) -> None:
+async def test_successful_voucher_login(
+    http_client: AsyncClient,
+    db_session: AsyncSession,
+    subtests: SubTests,
+) -> None:
     """Test that a valid voucher can login to the system and receive a JWT."""
     db_session.add(Voucher(voucher_code="k1p", balance="13.37"))
     await db_session.flush()
@@ -17,3 +22,11 @@ async def test_successful_voucher_login(http_client: AsyncClient, db_session: As
     resp_data: dict[str, str] = resp.json()
     assert resp.status_code == HTTPStatus.OK
     assert {"voucher_code", "jwt"} <= resp_data.keys()
+    jwt = resp_data["jwt"]
+
+    with subtests.test(msg="Test a valid voucher JWT can request own voucher info", jwt=jwt):
+        resp = await http_client.get("/vouchers/me", headers={"Authorization": f"Bearer {jwt}"})
+
+        assert resp.status_code == HTTPStatus.OK
+        resp_data: dict[str, str] = resp.json()
+        assert {"id", "created_at", "updated_at", "voucher_code", "balance", "active"} <= resp_data.keys()
