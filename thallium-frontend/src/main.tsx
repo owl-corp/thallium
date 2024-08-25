189c35b496a623f4a9c28b2f415533e23cce613c
Joe Banks <joe@jb3.dev>
2024-08-25 04:01:52 +0100

    Wrap app in Redux Provider

--- thallium-frontend/src/main.tsx
+++ thallium-frontend/src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "@fontsource-variable/fira-code";
+import { Provider } from "react-redux";
+import store from "./store";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
