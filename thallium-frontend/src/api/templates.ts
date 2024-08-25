79441282fb751553ffbd7af6145bb4a427f1723c
Joe Banks <joe@jb3.dev>
2024-08-25 04:03:35 +0100

    Add API module for templates

---
+++ thallium-frontend/src/api/templates.ts
@@ -0,0 +1,38 @@
+import { get } from "./client";
+import store from "../store";
+
+export interface Variant {
+    variant_id: number;
+    name: string;
+    size: string;
+    colour?: string;
+    colour_code?: string;
+    colour_code2?: string;
+    price: string;
+    last_synced: string;
+}
+
+export interface Template {
+    template_id: number;
+    title: string;
+    product_id: number;
+    mockup_file_url: string;
+    last_synced: string;
+    variants?: Variant[];
+}
+
+export const getTemplates = async (withVariants: boolean): Promise<Template[]> => {
+    /* TODO: Handle lacking authorization */
+
+    const token = store.getState().authorization.voucherToken;
+
+    if (!token) {
+        throw new Error("No token available");
+    };
+
+    return await get(`/templates/?with_variants=${withVariants.toString()}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    }) as unknown as Template[];
+};
