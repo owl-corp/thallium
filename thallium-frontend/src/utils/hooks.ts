4123687310c1788c5b1ff85be920e42ab24d1d96
Joe Banks <joe@jb3.dev>
2024-09-11 23:15:22 +0100

    Add useMediaQuery hook

--- thallium-frontend/src/utils/hooks.ts
+++ thallium-frontend/src/utils/hooks.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { type RefObject } from "react";
 
 export function useVisible(ref: RefObject<HTMLElement>) {
@@ -16,4 +16,24 @@ export function useVisible(ref: RefObject<HTMLElement>) {
   }, [ref]);
 
   return isVisible;
+}
+
+export function useMediaQuery(query: string) {
+  const queryObject = useMemo(() => {
+    return window.matchMedia(query);
+  }, [query]);
+
+  const [matches, setMatches] = useState(queryObject.matches);
+
+  useEffect(() => {
+    const listenHook = (e: MediaQueryListEvent) => {
+      setMatches(e.matches);
+    }
+
+    queryObject.addEventListener("change", listenHook);
+
+    return () => queryObject.removeEventListener("change", listenHook);
+  }, [queryObject]);
+
+  return matches;
 }
